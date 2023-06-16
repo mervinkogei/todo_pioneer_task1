2054e2fac6725f4bf23475d239c949fdc1e42431
mervinkogei <vincent.kogei@yahoo.com>
2023-06-16 21:55:03 +0300

    Add themestling and fonts

---
+++ lib/Utils/themeStyling.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeStyling{
+   static var logoStyle = GoogleFonts.montserrat(
+    fontSize: 28.0,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.2,
+  );
+}
