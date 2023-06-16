cf93b169a3d9532f7adaa63c1314d7a656c0662c
mervinkogei <vincent.kogei@yahoo.com>
2023-06-16 21:18:24 +0300

    Add constants colors

---
+++ lib/Utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color  redColor= Color(0xffDA4040);
+const Color  blueColor= Color(0xff5f52ee);
+const Color  blackColor= Color(0xff3a3a3a);
+const Color  greyColor= Color(0xff717171);
+const Color  backgroundColor= Color(0xffeeeff5);
+
